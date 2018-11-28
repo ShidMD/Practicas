9005f21bb961fd05043fe48c63394dbd7e2b8a77
Shift Genesis Angeal <Shidtmind@gmail.com>
2018-11-28 21:33:20 +0100

    Traducido main.old a cpp

--- ClaseZonaMemoria/main.cpp
+++ ClaseZonaMemoria/main.cpp
@@ -1,10 +1,45 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "ClaseZonaMemoria.h"
 #include <string.h> // para strcpy
-const unsigned int memorySize = 4096;
-unsigned char zonaReserva[memorySize];
+const unsigned int tamMem = 4096;
+unsigned char bytes[tamMem];
 
 int main() {
-	ClaseZonaMemoria memoria(memorySize,zonaReserva);
+	uint32_t * matrizEnteros;
+	int nEnteros = 10;
+	// Para manejar una matriz de enteros de 32 bits sin signo en la zona
+
+	double * pReal;
+	// Para manejar un real de 64 bits en la zona
+
+	char * mensaje;
+	int maxLongMensaje = 50;
+	// Para manejar una cadena de caracteres de hasta 50 bytes
+
+	ClaseZonaMemoria zona(tamMem, bytes);
+	// Inicializa la zona para manejar hasta 4096 bytes = 4 KB
+
+	if (!zona.reservaBloque(sizeof(uint32_t) * nEnteros,(void **)&matrizEnteros)) return -1;
+	// Reserva en la zona un bloque para manejar una matriz de enteros.
+	// Guarda en 'matrizEnteros' su dirección
+
+	if (!zona.reservaBloque(sizeof(double),(void **)&pReal)) return -1;
+	// Reserva en la zona un bloque para manejar un real de tipo double.
+	// Guarda su dirección en 'pReal'
+
+	if (!zona.reservaBloque(maxLongMensaje,(void **)&mensaje)) return -1;
+	// Reserva en la zona un bloque para manejar una cadena de 'maxLongMensaje' bytes.
+	// Guarda su dirección en 'mensaje'
+
+	for (int i = 0; i < nEnteros; i++)
+		matrizEnteros[i] = i;
+	*pReal = 33.33;
+	strcpy(mensaje, "aaaaa");
+	// Guarda información en la matriz de enteros, en el real y en la cadena
+	zona.liberaBloque((int16_t*)pReal);
+	// Libera de la zona el bloque ocupado por el real
+	if (zona.zonaMemoriaFragmentada())
+		zona.compactaZonaMemoria();
+	// Si la zona está fragmentada, la compacta
 	return 0;
 }
