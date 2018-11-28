a3d25b590f8e40f9c2c2a72ea3bd384f10556e78
Shift Genesis Angeal <Shidtmind@gmail.com>
2018-11-28 19:38:01 +0100

    Inicial ClaseZonaMemoria

---
+++ ClaseZonaMemoria/ClaseZonaMemoria.cpp
@@ -0,0 +1,61 @@
+#include "ClaseZonaMemoria.h"
+
+#include <stdio.h> // Para NULL
+#include <string.h> // Para strcpy y memcpy
+
+int8_t ClaseZonaMemoria::tamanoCabecera = sizeof(int16_t) + sizeof(void **);
+
+ClaseZonaMemoria::ClaseZonaMemoria(int16_t t, unsigned char * bytes) :
+
+	tamano(t),
+	pComienzo(bytes),
+	pSiguienteReserva(bytes),
+	fragmentada(false)
+{
+
+}
+
+int16_t ClaseZonaMemoria::sizeBlock(int16_t * pBloque) {
+	//devuelve el tamaño en valor absoluto de un puntero a un bloque
+	return (*pBloque < 0 ? *pBloque * -1 : *pBloque);
+}
+
+bool ClaseZonaMemoria::isFreeBlock(int16_t * pBloque) {
+	//devuelve si se puede escribir en el bloque apuntado
+	return (*(int16_t *)pBloque > 0);
+}
+
+bool ClaseZonaMemoria::reservaBloque(int16_t bSize, void ** pVar) {
+	if (bSize + this->tamanoCabecera + this->pSiguienteReserva > this->pComienzo + this->tamano) return false;
+	else {
+		*(int16_t *)this->pSiguienteReserva = bSize;
+		*(void **)(this->pSiguienteReserva + sizeof(int16_t)) = pVar;
+		*pVar = this->pSiguienteReserva + this->tamanoCabecera;
+		this->pSiguienteReserva += bSize + this->tamanoCabecera;
+		return 1;
+	}
+}
+
+void ClaseZonaMemoria::liberaBloque(int16_t * pValor) {
+	if (!this->isFreeBlock(pValor - this->tamanoCabecera)) {
+		*(pValor - this->tamanoCabecera) *= -1;
+		pValor = NULL;
+	}
+}
+
+void ClaseZonaMemoria::compactar() {
+
+
+}
+void ClaseZonaMemoria::borrar() {
+	unsigned int i = 0;
+	while (this->pComienzo + i < this->pComienzo + this->tamano && *(int16_t*)(this->pComienzo + i)) {
+		*(unsigned *)(*(unsigned **)(this->pComienzo + i + sizeof(int16_t))) = NULL;
+		i += *(int16_t *)(this->pComienzo + i) + this->tamanoCabecera;
+	}
+	memset(this->pComienzo, 0, this->tamano);
+}
+
+bool ClaseZonaMemoria::esFragmentada() {
+	return this->fragmentada;
+}
---
+++ ClaseZonaMemoria/ClaseZonaMemoria.h
@@ -0,0 +1,29 @@
+#ifndef CLASEZONAMEMORIA_H_
+#define CLASEZONAMEMORIA_H_
+#include <stdint.h>
+
+class ClaseZonaMemoria {
+private:
+	//no se utiliza el puntero pComienzo debido a que existe "this" que podemos utilizar para ese fin
+	unsigned int tamano;
+	unsigned char * pComienzo;
+	unsigned char * pSiguienteReserva;
+	bool fragmentada;
+
+	int16_t sizeBlock(int16_t *);
+	bool isFreeBlock(int16_t *);
+
+public:
+
+	static int8_t tamanoCabecera;
+
+	bool reservaBloque(int16_t, void **);
+	void liberaBloque(int16_t *);
+	bool esFragmentada();
+	void compactar();
+	void borrar();
+	ClaseZonaMemoria(int16_t, unsigned char *);
+
+};
+
+#endif 
